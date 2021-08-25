d4b7d4ce371ff04dc0ed4471d839b7463ea09256
Nicolas Foiny <nicolas.foiny@arrow.com>
2021-08-25 10:37:33 +0200

    Proxy for campaign

---
+++ src/Campaigns/CampaignsClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ArrowSphere\PublicApiClient\Campaigns;
+
+use ArrowSphere\PublicApiClient\AbstractClient;
+
+/**
+ * Class CampaignsClient
+ */
+class CampaignsClient extends AbstractClient
+{
+    public const TEST = 'TEST';
+}
