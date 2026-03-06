391bc853eb9e855634a8ef898b81d644eee627b8
Maher Landolsi <maher.landolsi@arrow.com>
2026-03-06 17:45:44 +0100

    add customFields to createOrder

---
+++ src/Orders/Request/SubEntities/CustomField.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ArrowSphere\PublicApiClient\Orders\Request\SubEntities;
+
+use ArrowSphere\PublicApiClient\Entities\AbstractEntity;
+use ArrowSphere\PublicApiClient\Entities\Property;
+
+class CustomField extends AbstractEntity
+{
+    public const COLUMN_LABEL = 'label';
+    public const COLUMN_VALUE = 'value';
+
+    #[Property(required: true)]
+    protected string $label;
+
+    #[Property(required: true)]
+    protected string $value;
+}
